150dc8d64f4bb455441e8c625377fcc39297f80d
Jameson Marshall <jamesonhmarshall@gmail.com>
2023-10-10 21:02:24 +0000

    ch2 proj 1

--- modern_c/ch2/ch2.c
+++ modern_c/ch2/ch2.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
 
+// function declarations
 void e1(void);
+void p1(void);
+
 
 int main(void) {
-    e1();
+    // e1();
+    p1();
 }
 
+
+// FUNCTIONS
 void e1(void){
     printf("hello world\n");
 }
+
+void p1(void) {
+    // prints a "Check mark"
+    int rows = 6, cols = 8;
+    char c = '*';
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols - 1 - i; j++) {
+            if (i >= 3 && j == i-3){
+                printf("%c", c);
+            } else {
+                printf(" ");
+            }
+        }
+        printf("%c\n", c);
+    }
+}
+
